1b3dd72cc22bfaeaf4e46caff2f066eac07af4f2
Enis Yavuz <enis.1739@gmail.com>
2023-01-29 15:34:19 +0300

    Delete src/Components directory

--- src/Components/Search/Search.style.js
+++
@@ -1,9 +0,0 @@
-import { StyleSheet } from 'react-native';
-
-export default styles= StyleSheet.create({
-    container: {
-        backgroundColor:"#e0e0e0",
-        borderRadius:20,
-        marginTop:5,
-    }
-});
